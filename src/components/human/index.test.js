bd84ed6af09de0384178fd638496d9551fcfe76a
Nate <7nater.nelson@gmail.com>
2022-02-01 09:07:49 -0700

    Added sample file

--- src/components/human/index.test.js
+++
@@ -1,8 +0,0 @@
-import component from '../components/index'
-
-describe("This is a dummy test", ()=>{
-    it("should be able to run", () => {
-        expect("This to be true").toBeTruthy()
-        expect(component()).toEqual("Foo - Bar")
-    })
-})
