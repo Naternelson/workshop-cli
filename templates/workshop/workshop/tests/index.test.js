45a344a8d216bb3f2fb6c21ae1e71de1bf3c016c
Nate <7nater.nelson@gmail.com>
2022-02-02 11:52:55 -0700

    changes made

--- templates/workshop/workshop/tests/index.test.js
+++ templates/workshop/workshop/tests/index.test.js
@@ -1,8 +1,1 @@
-import component from "../units/index.js"
-
-describe("This is a dummy test", ()=>{
-    it("should be able to run", () => {
-        expect("This to be true").toBeTruthy()
-        expect(component()).toEqual("Foo - Bar")
-    })
-})
+// Test File
--- templates/workshop/workshop/units/index.js
+++ templates/workshop/workshop/units/index.js
@@ -1,3 +1,1 @@
-export default function component(){
-    return "Foo - Bar"
-}
+// unit file
