ce1d354a7b18c3ce63d3373fa17528afa0651d62
Nate <7nater.nelson@gmail.com>
2022-02-03 18:36:16 -0700

    commit

--- src/components/init/init.js
+++ src/components/init/init.js
@@ -4,14 +4,59 @@
 const PackageHandler = require("../package-handler/package-handler.js")
 const path = require("path")
 const fs = require("fs-extra")
+const inquirer = require("inquirer")
+const Listr = require("listr")
 
 module.exports =  async function init(options){
-    setupDirs()
+    await setupDirs()
     await setupPackageFile(options)
+    const responses = await promptMissingQuestions(options)
+    const tasks = new Listr([
+        {
+            title: 'Initalizing Git',
+            enabled: () => responses.git,
+            task: () => 
+        }
+    ])
 }
 
-async function askQuestions(questions){
-    
+async function promptMissingQuestions(options){
+    const questions = []
+    const keys = ["git", "gitBranch", "feature", "component"]
+    const {git, gitBranch, feature, component} = options
+    if(!git) questions.push(promptCreator({name: 'git', type: 'confirm', message: "Initalize Git?"}))
+    if(!gitBranch) questions.push(promptCreator({name: 'gitBranch', type: 'confirm', message: 'Auto setup git branches on feature change?'}))
+    if(!feature) questions.push(promptCreator({name: 'feature', message: 'Name of first feature'}))
+    if(!component) questions.push(promptCreator({name: 'component', message: 'Name of first component'}))
+    return await askQuestions(questions)
+}
+
+function promptCreator(params){
+    let {type, name, message, defaultValue, choices, validate, filter, pageSize, prefix, askAnswered, loop, suffix} = params
+    switch(type){
+        case "list":
+            if(!choices) return 
+            break 
+        case undefined: 
+            type = "input"
+            break 
+    }
+    if(!name || !message) return null 
+    return {type, name, message, defaultValue, choices, validate, filter, pageSize, prefix, askAnswered, loop, suffix}
+
+}
+
+async function askQuestions(...questions){
+    const allowedKeys = ["type", "name", "message", "default", "choices", "validate", "filter", "pageSize", "prefix", "askAnswered", "loop"]
+    const filterdQs = questions.reduce((newQuestions, question) => {
+        const thisQuestion = {}
+        for(let key in question){
+            if(allowedKeys.includes(key))thisQuestion[key] = question[key]
+        }
+        newQuestions.push(thisQuestion)
+        return newQuestions
+    }, [])
+    return  await inquirer(filterdQs)
 }
 
 
